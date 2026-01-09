4b2f2c6d66c33fd0e323ac597fb3f0ec1a2401fa
Active Syntax <dev@activesyntax.net>
2026-01-09 05:50:22 +0100

    Branding file added

---
+++ src/app/shared/branding/branding.ts
@@ -0,0 +1,11 @@
+export const Branding = {
+  site: {
+    name = 'EB Clean',
+    slogan: 'Professional surface cleaning',
+    title: 'EB Clean - Professional surface cleaning',
+  },
+  logo: {
+    src: 'assets/img/branding/eb-clean-logo.svg',
+    alt: 'EB Clean - Professional surface cleaning'
+  }
+};
