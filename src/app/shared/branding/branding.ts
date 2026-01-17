58bd22e7a25a5065982ae763d239763383f21d3d
Active Syntax <dev@activesyntax.net>
2026-01-17 09:55:28 +0100

    slogan design

--- src/app/shared/branding/branding.ts
+++ src/app/shared/branding/branding.ts
@@ -2,6 +2,7 @@ export const Branding = {
   site: {
     name: 'EB Clean',
     slogan: 'Gondos tisztítás, látható eredmény.',
+    sloganHtml: 'Gondos <span class="text-emphasis">tisztítás</span>, látható <span class="text-emphasis">eredmény</span>.',
     title: 'EB Clean - Gondos tisztítás, látható eredmény.',
     description: 'Napelemek, falfelületek, térkövek, síremlékek professzionális tiszítása. '
   },
