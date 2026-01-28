2489f981cfa0c3f0fae7a732a5ab1eeb9e483730
Active Syntax <dev@activesyntax.net>
2026-01-28 13:09:28 +0100

    references and advantages improvements

--- src/app/home/references/references.ts
+++ src/app/home/references/references.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
-import {Gallery} from '../../gallery/gallery'
-
+import { Gallery, GalleryImage } from '../../gallery/gallery'; // Importáljuk az interfészt is
 
 @Component({
   selector: 'app-home-references',
+  standalone: true,
   imports: [Gallery],
   templateUrl: './references.html',
   styleUrl: './references.css',
 })
 export class References {
- folkfiokImages = [
+  
+  // 1. Általános munkálatok galéria (a generált képek)
+  generalImages: GalleryImage[] = [
+    {
+      large: '/assets/img/references/napelem-tisztitas.png',
+      thumb: '/assets/img/references/napelem-tisztitas.png',
+      width: 1536,
+      height: 1024
+    },
+    {
+      large: '/assets/img/references/faltisztitas.png',
+      thumb: '/assets/img/references/faltisztitas.png',
+      width: 1024,
+      height: 1024
+    },
+    {
+      large: '/assets/img/references/terko-tisztitas.png',
+      thumb: '/assets/img/references/terko-tisztitas.png',
+      width: 1024,
+      height: 1024
+    },
+    {
+      large: '/assets/img/references/siremlek-tisztitas.png',
+      thumb: '/assets/img/references/siremlek-tisztitas.png',
+      width: 1024,
+      height: 1024
+    }
+  ];
+
+  // 2. Előtte-Utána galéria (a meglévő folkfiok referenciák)
+  beforeAfterImages: GalleryImage[] = [
     {
       large: '/assets/img/references/folkfiok-01-01.jpeg',
       thumb: '/assets/img/references/thumbs/folkfiok-01-01.jpeg',
@@ -33,5 +63,6 @@ export class References {
       thumb: '/assets/img/references/thumbs/folkfiok-03.jpeg',
       width: 800,
       height: 668
-    }  ];
+    }
+  ];
 }
