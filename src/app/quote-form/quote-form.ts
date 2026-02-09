7c933403927f115b39d8490a90e1d1c898de2b80
Active Syntax <dev@activesyntax.net>
2026-02-09 08:28:54 +0100

    read quote template form json

--- src/app/quote-form/quote-form.ts
+++ src/app/quote-form/quote-form.ts
@@ -1,35 +1,30 @@
-import { Component,  inject } from '@angular/core';
+import { Component,  inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-quote-form',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './quote-form.html',
   styleUrl: './quote-form.css',
 })
-export class QuoteForm {
+export class QuoteForm implements OnInit {
 
   private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
 
   contactForm: FormGroup;
 
   constructor() {
-const pattern = `Tisztelt EB-Clean!
-
-Szeretnék árajánlatot kérni egy családi ház napelem tisztítására Bakonyjákón. A rendszer 12 paneles.
-
-Üdvözlettel: [név]
-Tel: +36 
-Email: 
-`;
-
-this.contactForm = this.fb.group({
-  message: [pattern, [Validators.required, Validators.minLength(50)]]
-});    // Form inicializálása a mintával
-
     this.contactForm = this.fb.group({
-      message: [pattern, [Validators.required, Validators.minLength(20)]]
+      message: ['', [Validators.required, Validators.minLength(20)]]
+    });
+  }
+
+  ngOnInit() {
+    this.http.get<{quoteTemplateMessage: string}>('assets/data/messages.json').subscribe(data => {
+      this.contactForm.get('message')?.setValue(data.quoteTemplateMessage);
     });
   }
 
