8035cc56b225243631c5cbf5fa7d6be76890ca11
Active Syntax <dev@activesyntax.net>
2026-03-02 22:37:06 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Node development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.nodejs_24
+        pkgs.git
+      ];
+
+      shellHook = ''
+        echo "Dev shell loaded"
+      '';
+    };
+  };
+}
